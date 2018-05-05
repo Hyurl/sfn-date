a8576f25bbda916e9f3244f7e291894a78cffff5
hyurl <i@hyurl.com>
2018-05-05 17:21:55 +0800

    support Nodejs4

--- date.js
+++ date.js
@@ -35,21 +35,25 @@ function date(format, timestamp) {
         timestamp = format;
         format = null;
     }
+
     var format = format || 'Y-m-d H:i:s',
         _date = new Date();
+
     if (timestamp) _date.setTime(timestamp);
 
     var year = [
         _date.getFullYear(),
         _date.getFullYear().toString().substr(2)
-    ],
-        month = [
-            ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-            ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
-            ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        ],
-        date = _date.getDate() >= 10 ? _date.getDate() : '0' + _date.getDate(),
+    ];
+
+    var month = [
+        ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
+        ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    ];
+
+    var date = _date.getDate() >= 10 ? _date.getDate() : '0' + _date.getDate(),
         hours = _date.getHours() >= 10 ? _date.getHours() : '0' + _date.getHours(),
         minutes = _date.getMinutes() >= 10 ? _date.getMinutes() : '0' + _date.getMinutes(),
         seconds = _date.getSeconds() >= 10 ? _date.getSeconds() : '0' + _date.getSeconds(),
@@ -61,9 +65,9 @@ function date(format, timestamp) {
         apm = hours <= 12 ? ['AM', 'am'] : ['PM', 'pm'];
 
     if (ms < 10) {
-        ms = `00${ms}`;
+        ms = "00" + ms;
     } else if (ms < 100) {
-        ms = `0${ms}`;
+        ms = "0" + ms;
     }
 
     format = format.replace(/\bgmt|utc\b/i, _date.toUTCString())
@@ -109,6 +113,7 @@ date.tick = function tick(format, callback, interval) {
     } else {
         interval = interval || 1000;
     }
+
     var start = 999 - (new Date).getMilliseconds(),
         timer = null,
         tick = function () {
@@ -116,7 +121,8 @@ date.tick = function tick(format, callback, interval) {
             if (result === false)
                 clearInterval(timer);
         };
-    setTimeout(() => {
+
+    setTimeout(function () {
         timer = setInterval(tick, interval);
         tick();
     }, start);
@@ -125,7 +131,8 @@ date.tick = function tick(format, callback, interval) {
 if (typeof module === 'object' && module.exports) {
     module.exports = date; // CommonJS
 } else if (typeof define === 'function') {
-    define((require, exports, module) => {
-        module.exports = date; // AMD
+    // AMD/CMD
+    define(function (require, exports, module) {
+        module.exports = date;
     });
 }
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const date = require('./');
 
-console.log("Y-m-d H:i:s", date("Y-m-d H:i:s"));
-console.log("W M d Y H:i:s", date("W M d Y H:i:s"));
-console.log("utc", date("utc"));
-console.log("iso", date("iso"));
+console.log("Y-m-d H:i:s =>", date("Y-m-d H:i:s"));
+console.log("W M d Y H:i:s =>", date("W M d Y H:i:s"));
+console.log("utc =>", date("utc"));
+console.log("iso =>", date("iso"));
 
 var count = 0;
 console.log("\nStart ticking (Y-m-d H:i:s.ms)...");
